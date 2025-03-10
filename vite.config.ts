5398af490f79122b8313038ae6d69c2eb04772b4
jagadishkesav2 <jagadish@navadhiti.com>
2025-03-10 11:52:12 +0530

    Fix the base path of vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/leave-management-system/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
